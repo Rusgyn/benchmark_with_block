0f2ec3b98c8a4458cb35a5fe984b86e19fd80779
rusgyn <rusgynmagsanay@gmail.com>
2024-10-26 14:11:19 -0400

    Remove project instruction comment

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,15 +1,11 @@
 def benchmark
-
   start_time = Time.now
   yield # Execute the block of code passed to `benchmark`
   end_time = Time.now
 
   end_time - start_time
-
 end
  
-# Be careful, pasting this into IRB will take a long time to print.
-# It's a loooong string. :)
 long_string = "apple"*100000000
  
 running_time = benchmark { long_string.reverse }
